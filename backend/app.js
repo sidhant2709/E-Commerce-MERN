d7843ac5653e2ec76fbc7e686df09d5e166bcd65
sidhant <sidhant1593@gmail.com>
2021-07-04 13:44:22 +0530

    pagination search completed

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const errorMiddleWare = require("./middlewares/errors");
+
 app.use(express.json());
 
 //Import all the routes
@@ -8,5 +10,8 @@ app.use(express.json());
 const products = require("./routes/product");
 
 app.use("/api/v1", products);
+
+//Middleware to handle error
+app.use(errorMiddleWare);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
 const app = require("./app");
+const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 
-// Setting up config files
+//Handle the uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR ${err.message}`);
+  console.log(`Shutting down due to uncaught exception`);
+  process.exit(1);
+});
 
+// Setting up config files
 dotenv.config({ path: "backend/config/config.env" });
 
-app.listen(process.env.PORT, () => {
+//Connecting to Database
+connectDatabase();
+
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Listening to Server on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
   );
+});
+
+//Handle Unhandled Promise Rejections
+
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
